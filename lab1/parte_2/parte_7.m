5a9142be47acaecab613f31009863ec56a79ccdc
Toni Cabrera <cabreratony4@gmail.com>
2017-09-10 18:22:59 -0300

    Agrega los problemas reales

---
+++ lab1/parte_2/parte_7.m
@@ -0,0 +1,6 @@
+more off;
+
+% cargo el problema
+sistema = load('./../sistemas_reales/nasa1824');
+A = sistema.Problem.A;
+b = sistema.Problem.b;
---
+++ lab1/parte_2/parte_8.m
@@ -0,0 +1,6 @@
+more off;
+
+% cargo el problema
+sistema = load('./../sistemas_reales/contenedores');
+A = sistema.Problem.A;
+b = sistema.Problem.b;
