ff3699bbd21611f3060a6b2c1cc9be2609fc36b2
Toni Cabrera <cabreratony4@gmail.com>
2017-09-17 00:40:42 -0300

    Add sor and jor

---
+++ lab1/algoritmos/jor.m
@@ -0,0 +1,41 @@
+function [x_sol, cant_iter, xk_suc] = jor(A, b, x0, w, max_iter, tolerancia)
+  if nargin ~= 5
+    error('Ingrese todos los parametros');
+  end
+
+  [m, n] = size(A);
+
+  if n ~= m
+    error('La matriz debe ser cuadrada');
+  end
+
+  k = 0;
+  x_anterior = x0;
+  xk = zeros(n, 1);
+  error_absoluto = inf;
+  xk_suc = [];
+
+  while (k < max_iter & error_absoluto > tolerancia)
+
+    # calculo de jacobi en filas
+    for i = 1:n
+      suma_fila = A(i,:) * x_anterior;
+
+      jacobi = x_anterior(i) + (b(i) - suma_fila) / A(i, i);
+
+      % jor
+      xk(i) = jacobi * w + (1 - w) * x_anterior(i);
+    end
+
+    # avanzo el iterador
+    k++;
+
+    # actualizacion de condicion y solucion
+    error_absoluto = norm(A * xk - b);
+    x_anterior = xk;
+    xk_suc = [xk_suc, xk];
+  end
+
+  cant_iter = k;
+  x_sol = xk;
+end
---
+++ lab1/algoritmos/sor.m
@@ -0,0 +1,52 @@
+function [x_sol, cant_iter, xk_suc] = gauss_seidel(A, b, x0, w, max_iter, tolerancia)
+  more off;
+
+  if nargin ~= 5
+    error('Ingrese todos los parametros');
+  end
+
+  [m, n] = size(A);
+
+  if n ~= m
+    error('La matriz debe ser cuadrada');
+  end
+
+  k = 0;
+  x_anterior = x0;
+  xk = zeros(n, 1);
+  error_absoluto = inf;
+  xk_suc = []
+
+  while (k < max_iter & error_absoluto > tolerancia)
+
+    # calculo de jacobi en filas
+    for i = 1:n
+      suma_rapida = 0;
+      suma_as_jacobi = 0;
+
+      for j = 1:i-1
+        suma_rapida += A(i,j) * xk(j);
+      end
+
+      for j = i+1:n
+        suma_as_jacobi += A(i,j) * x_anterior(j);
+      end
+
+      gauss_seidel = (b(i) - suma_rapida - suma_as_jacobi) / A(i, i);
+
+      xk(i) = gauss_seidel * w + (1 - w) * x_anterior(i);
+    end
+
+    # avanzo el iterador
+    k++;
+
+    # actualizacion de condicion y solucion
+    error_absoluto = norm(A * xk - b)
+
+    x_anterior = xk;
+    xk_suc = [xk_suc, xk];
+  end
+
+  cant_iter = k;
+  x_sol = xk;
+end
