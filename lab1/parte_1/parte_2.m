d447b20069304755c9fe90fe96093716ecb321a8
Alejandro Barreiro <alebarreiro91@gmail.com>
2017-09-18 21:02:12 -0300

    Refactor parte1 -> parte 2

--- lab1/parte_1/parte_2.m
+++ lab1/parte_1/parte_2.m
@@ -1,3 +1,26 @@
 more off;
 clear;
 format long;
+
+% Resolver el sistema Ax = b mediante escalerizaci ́on Gaussiana junto con
+% sustitucion hacia atras. Reportar matriz triangular superior, solucion y tiempo
+% de ejecucion.
+
+addpath(genpath('./../algoritmos'));
+
+A = [29 34 -10; 34 41 -12; -10 -12 24];
+b = [1; 2; 3];
+
+main_tic = tic()
+
+tic()
+[H, L, b] = eg(A,b);
+toc()
+
+tic()
+x = sustitucion_hacia_atras(H, b)
+toc()
+
+toc(main_tic)
+
+spy(H);
