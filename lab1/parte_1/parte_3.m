fbb8b545d321d338a7d693b65c07ab9a98ff8970
Alejandro Barreiro <alebarreiro91@gmail.com>
2017-09-18 21:02:45 -0300

    Va el cholo

--- lab1/parte_1/parte_3.m
+++ lab1/parte_1/parte_3.m
@@ -1,3 +1,33 @@
 more off;
 clear;
 format long;
+
+% Resolver utilizando la descomposicion de Cholesky junto con sustitucion hacia
+% atras y hacia adelante. Reportar matriz H (superior), solucion y tiempo de
+% ejecucion.
+
+addpath(genpath('./../algoritmos'));
+
+A = [29 34 -10; 34 41 -12; -10 -12 24];
+b = [1; 2; 3];
+
+main_tic = tic()
+
+tic()
+L_trans = chol(A) %Matriz triangular superior (H)
+toc()
+
+L = L_trans'
+
+% Ax = b -> LL'x = b
+% Ly = b    % sustitucion hacia adelante
+% L'x = y   % sustitucion hacia atras
+
+tic()
+y = sustitucion_hacia_adelante(L, b);
+x = sustitucion_hacia_atras(L_trans, y)
+toc()
+
+toc(main_tic)
+
+spy(L_trans)
