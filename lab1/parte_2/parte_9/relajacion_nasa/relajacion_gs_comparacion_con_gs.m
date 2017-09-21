b2f0065133c691956603119369ea9f19c9ee22e0
Toni Cabrera <cabreratony4@gmail.com>
2017-09-20 21:28:39 -0300

    Agrego SOR vs gs

---
+++ lab1/parte_2/parte_9/relajacion_nasa/relajacion_gs_comparacion_con_gs.m
@@ -0,0 +1,48 @@
+more off;
+
+clear;
+format long;
+
+% cargo los algoritmos
+addpath(genpath('./../../../algoritmos'));
+
+sistema = load('./../../../sistemas_reales/nasa1824');
+A = sistema.Problem.A;
+b = sistema.Problem.b;
+
+n = 500;
+
+tic()
+[l, m, r] = gauss_seidel(A, b, zeros(length(b), 1), n, 0.00000001);
+toc()
+
+tic()
+[ll, mm, rr] = sor(A, b, zeros(length(b), 1), 1.84101, n, 0.00000001);
+toc()
+
+% for i = 1:n
+%
+% end
+
+hold on;
+grid on;
+
+sol_octave = A \ b;
+
+error_absoluto_gs = zeros(n, 1);
+error_absolut_sor = [];
+distancia_entre_ellos = [];
+
+for i = 1:n
+  error_absoluto_gs(i) = norm(r(:, i) - sol_octave);
+  error_absolut_sor(i) = norm(rr(:, i) - sol_octave);
+end
+
+plot(1:n, error_absoluto_gs, 'b');
+plot(1:n, error_absolut_sor, 'g');
+ylabel('Error absoluto');
+xlabel('Cantidad de iteraciones');
+legend('Gauss-Seidel', 'SOR', 'location', 'northwest');
+
+% axis ('labely','tic');
+% set(gca,'yTickLabel',[10^-1 10^-2 10^-3 10^-4 10^-5] );
