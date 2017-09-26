89dbc9fdcb0ff5af2ad896f8389f644e9a586b93
Alvaro Badaracco <abadaraccopagano@gmail.com>
2017-09-26 11:57:09 -0300

    versiones finales

--- lab1/parte_1/parte4.m
+++ lab1/parte_1/parte4.m
@@ -3,20 +3,43 @@ addpath(genpath('./../algoritmos'));
 
 A = [29 34 -10; 34 41 -12; -10 -12 24];
 
-D = [29 0 0; 0 41 0; 0 0 24];
-
 b = [1; 2; 3];
 
-D_inv = inv(D);
+q_j = q_jacobi(A)
 
-LU = [0 34 -10; 34 0 -12; -10 -12 0];
+r_j = r_jacobi(A, b)
 
-QJ = -D_inv * LU
+vp = eig(q_jacobi(A))
 
-RJ = D_inv * b
-
-vp = eig(QJ)
+radioEspectral = radio_espectral(q_jacobi(A))
 
 x_inicial = [0; 0; 0];
 
-[x, cantIter, xk] = jacobi(A, b, x_inicial, 100, 0.001)
+tic();
+
+[x, cantIter, xk] = jacobi(A, b, x_inicial, 100, 0.001);
+
+tiempo_de_ejecucion = toc()
+x
+
+sol_octave = A\b
+
+error_absoluto_jacobi = zeros(cantIter, 1);
+error_xk = zeros(cantIter, 1);
+
+for i = 1:cantIter
+   error_absoluto_jacobi(i) = norm(xk(:, i) - sol_octave);
+   error_xk(i) = norm(A * xk(:, i) - b);
+end
+
+plot(1:cantIter, error_absoluto_jacobi, 'b');
+ylabel('Error absoluto');
+xlabel('Cantidad de iteraciones');
+legend('Jacobi', 'location', 'northwest');
+ 
+figure;
+
+plot(1:cantIter, error_xk, 'b');
+ylabel('Residuo');
+xlabel('Cantidad de iteraciones');
+legend('Jacobi', 'location', 'northwest');
--- lab1/parte_1/parte5.m
+++ lab1/parte_1/parte5.m
@@ -3,20 +3,44 @@ addpath(genpath('./../algoritmos'));
 
 A = [29 34 -10; 34 41 -12; -10 -12 24];
 
-LD = [29 0 0; 39 41 0; -10 -12 24];
-
 b = [1; 2; 3];
 
-LD_inv = -inv(LD);
+q_gs = q_gauss_seidel(A)
 
-U = [0 34 -10; 30 0 -12; 0 0 0];
+r_gs = r_gauss_seidel(A, b)
 
-QGS = -LD_inv * U
+vp = eig(q_gauss_seidel(A))
 
-RGS = LD_inv * b
-
-vp = eig(QGS)
+radioEspectral = radio_espectral(q_gauss_seidel(A))
 
 x_inicial = [0; 0; 0];
 
-[x, cantIter, xk] = gauss_seidel(A, b, x_inicial, 100, 0.001)
+tic();
+
+[x, cantIter, xk] = gauss_seidel(A, b, x_inicial, 100, 0.001);
+
+tiempo_de_ejecucion = toc()
+
+x
+
+sol_octave = A\b
+
+error_absoluto_gs = zeros(cantIter, 1);
+error_xk = zeros(cantIter, 1);
+
+for i = 1:cantIter
+   error_absoluto_gs(i) = norm(xk(:, i) - sol_octave);
+   error_xk(i) = norm(A * xk(:, i) - b);
+end
+
+plot(1:cantIter, error_absoluto_gs, 'b');
+ylabel('Error absoluto');
+xlabel('Cantidad de iteraciones');
+legend('Gauss-Seidel', 'location', 'northwest');
+ 
+figure;
+
+plot(1:cantIter, error_xk, 'b');
+ylabel('Residuo');
+xlabel('Cantidad de iteraciones');
+legend('Gauss-Seidel', 'location', 'northwest');
