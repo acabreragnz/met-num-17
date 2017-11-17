514f9b8edf781846b56dca55c8d54c1bb0651779
AndresVidalB <31743183+AndresVidalB@users.noreply.github.com>
2017-11-17 16:24:45 -0300

    Actualización de Redondeo de Hilbert
    Ahora la función devuelve coeficientes en la base de newton.

--- lab2/algoritmos/redondeo_hilbert.m
+++ lab2/algoritmos/redondeo_hilbert.m
@@ -1,26 +1,18 @@
 
 #Recibe un polinomio escrito segun una base de Newton
 #convierte los coeficientes a la base de Hilbert, redondea y
-#devuelve los coeficientes del polinomio en la base canónica
+#devuelve los coeficientes del polinomio en la base de newton
 function coef = redondeo_hilbert(coeficientes_newton)
-  
+ 
   n = columns(coeficientes_newton)-1
 
-  # Obtener las bases de Newton y Hilbert adecuadas para este polinomio
-  base_newton = base_de_newton(n); 
-  base_hilbert = base_de_hilbert(n);
+  # Cálculo de la matriz de cambio de base de Newton a Hilbert (hIc * cIn)
+  hIn = inv(base_de_hilbert(n)) * base_de_newton(n);
   
-  # Definir las matrices para aplicar el teorema de cambio de base
-  hIc = inv(base_hilbert);
-  cIn = base_newton;
-  
-  # Cambio de base
-  hIn = hIc * cIn;
-  
-  # Cambio de coordenadas de Newton a Hilbert
-  coeficientes_hilbert = hIn * coeficientes_newton.';
-
-  # Cambio de coordenadas de Hilbert a Canónica
-  coef = base_hilbert*round(coeficientes_hilbert);
+  # Se calculan y redondean los coeficientes en la base de Hilbert,
+  # obtenidos mediante cambio de coordenadas Newton->Hilbert.
+  # Luego, se reaplica el cambio de coordenadas Newton->Hilbert
+  # para devolver el polinomio expresado en la base de newton.
+  coef = inv(hIn)*round(hIn * coeficientes_newton.';);
   
 endfunction
