f337e8257d01b5e1df5d798b99826a3863561e7e
AndresVidalB <31743183+AndresVidalB@users.noreply.github.com>
2017-11-17 17:07:53 -0300

    Add files via upload

---
+++ lab2/algoritmos/redondeo_hilbert_viejo.m
@@ -0,0 +1,25 @@
+#Recibe un polinomio escrito segun una base de Newton
+#convierte los coeficientes a la base de Hilbert, redondea y
+#devuelve los coeficientes del polinomio en la base canónica
+function coef = redondeo_hilbert_viejo(coeficientes_newton)
+  
+  n = columns(coeficientes_newton)-1
+
+  # Obtener las bases de Newton y Hilbert adecuadas para este polinomio
+  base_newton = base_de_newton(n); 
+  base_hilbert = base_de_hilbert(n);
+  
+  # Definir las matrices para aplicar el teorema de cambio de base
+  hIc = inv(base_hilbert);
+  cIn = base_newton;
+  
+  # Cambio de base
+  hIn = hIc * cIn;
+  
+  # Cambio de coordenadas de Newton a Hilbert
+  coeficientes_hilbert = hIn * coeficientes_newton.';
+
+  # Cambio de coordenadas de Hilbert a Canónica
+  coef = inv(hIn)*round(coeficientes_hilbert);
+  
+endfunction
