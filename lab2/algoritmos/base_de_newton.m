009fec4aa6575bd117bc962d6e4aeea7becb7f46
AndresVidalB <31743183+AndresVidalB@users.noreply.github.com>
2017-11-17 17:00:37 -0300

    Add files via upload

--- lab2/algoritmos/base_de_newton.m
+++ lab2/algoritmos/base_de_newton.m
@@ -7,17 +7,7 @@ function base = base_de_newton(n)
   
   for i = 2:n+1
     
-    # Productoria de (x-1)...(x-n)
-    y = zeros(1, n+1);
-    y(1, 1:2) = [1 -1]
-    for j = 2:n
-      factor = zeros(1, n+1)
-      factor(1, 1:2) = [1 -j]
-      y = conv(y,factor);
-    endfor
-    y = y(1,1:n+1);
-    
-    base(i,1:i) = y;
+    base(i,1:i) = productoria_de_newton(i-1);
 
   endfor
   
